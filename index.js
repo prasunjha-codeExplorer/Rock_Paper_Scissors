2209e2cb95efb64747ac4597e107cf483147b2e5
prasunjha-codeExplorer <prasunjha9431@gmail.com>
2025-07-04 10:55:58 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,97 @@
+let btn1 = document.querySelector("#rock");
+let btn2 = document.querySelector("#paper");
+let btn3 = document.querySelector("#scissors");
+let userScore = document.querySelector("#user");
+let compScore = document.querySelector("#comp");
+let countUserScore=0;
+let countCompScore=0;
+let p = document.querySelector(".display_msg");
+let winnerMsg = document.querySelector(".finalWinner");
+const finalWinner = (userChoice,compChoice) => {
+           // User Win pattern!
+           console.log(userChoice,compChoice);
+     p.style.fontSize = "2rem";
+    p.style.color = "black";
+    p.style.fontWeight = "bolder";
+    if(userChoice===compChoice){
+        p.innerText = `[Same choice by both players!]`;
+    }else{
+        p.innerText = `[user: ${userChoice} , comp: ${compChoice}]`;
+    }
+    if(userChoice==="rock" && compChoice==="scissors"){
+        userScore.innerText = ++countUserScore;
+    }
+    else if(userChoice==="scissors" && compChoice==="rock"){
+        compScore.innerText = ++countCompScore;
+    }
+    else if(userChoice==="paper" && compChoice==="rock"){
+        userScore.innerText = ++countUserScore;
+    }
+    else if(userChoice==="rock" && compChoice==="paper"){
+        compScore.innerText = ++countCompScore;
+    }
+    else if(userChoice==="scissors" && compChoice==="paper"){
+        userScore.innerText = ++countUserScore;
+    }
+    else if(userChoice==="paper" && compChoice==="scissors"){
+        compScore.innerText = ++countCompScore;
+    }
+     // Comp Win Pattern!
+    else if(compChoice==="rock" && userChoice==="scissors"){
+        compScore.innerText = ++countCompScore;
+     }
+     else if(compChoice==="scissors" && userChoice==="rock"){
+        userScore.innerText = ++countUserScore;
+     }
+     else if(compChoice==="paper" && userChoice==="rock"){
+        compScore.innerText = ++countCompScore;
+     }
+     else if(compChoice==="rock" && userChoice==="paper"){
+        userScore.innerText = ++countUserScore;
+     }
+     else if(compChoice==="scissors" && userChoice==="paper"){
+        compScore.innerText = ++countCompScore;
+     }
+     else if(compChoice==="paper" && userChoice==="scissors"){
+        userScore.innerText = ++countUserScore;
+     }
+    
+}
+let turns = 0;
+const playGame = (userChoice) => {
+     let random = ["rock","paper","scissors"];
+     let randomNum = (Math.floor)(Math.random() * 3);
+     let compChoice = random[randomNum];
+     finalWinner(userChoice,compChoice);
+     turns++;
+     if(turns==5){
+       winnerMsg.style.fontSize = "2rem";
+       winnerMsg.style.color = "#B9375E";
+       winnerMsg.style.fontWeight = "bolder";
+       winnerMsg.style.textAlign = "center";
+       winnerMsg.style.marginTop = "30px";
+       let gameWinner = "Both have equal scores!";
+        if(countUserScore > countCompScore){gameWinner="You have won the game!";}
+       else if(countCompScore > countUserScore){gameWinner="Computer has won the game!";}
+       winnerMsg.innerText = `After considering best of 5, ${gameWinner}`;
+       btn1.disabled = true;
+       btn2.disabled = true;
+       btn3.disabled = true;
+     }
+}
+
+
+    btn1.addEventListener("click", () => {
+    let userChoice = btn1.getAttribute("id");
+     playGame(userChoice);
+});
+
+btn2.addEventListener("click", () => {
+    let userChoice = btn2.getAttribute("id");
+    playGame(userChoice);
+});
+
+btn3.addEventListener("click", () => {
+    let userChoice = btn3.getAttribute("id");
+    playGame(userChoice);
+});
